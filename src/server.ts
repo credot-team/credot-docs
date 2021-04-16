3364e09b0f1fad3a4a5e0cebc23a58b5d3aa4e39
haneyume <haneyume@gmail.com>
2021-04-16 18:50:00 +0800

    update

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import session from 'express-session';
+import morgan from 'morgan';
 
 declare module 'express-session' {
   interface SessionData {
@@ -20,7 +21,31 @@ app.use(
   }),
 );
 
-app.use((req, res, next) => {}, express.static(process.cwd() + '/public'));
+app.use(morgan('dev'));
+
+app.post('/api/login', express.json(), (req, res, next) => {
+  const { account, password } = req.body;
+
+  console.log(account, password);
+
+  if (account === 'admin' && password === '123456') {
+    req.session.user = 'admin';
+    res.json({ code: 0 });
+  } else {
+    res.status(401).json({ code: 401 });
+  }
+});
+
+app.use(express.static(process.cwd() + '/src/public'));
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    res.sendFile(process.cwd() + '/src/index.html');
+    return;
+  }
+
+  next();
+}, express.static(process.cwd() + '/docusaurus/build'));
 
 app.listen(process.env.PORT, () => {
   console.log(new Date(), '🚀 Server ready!');
